7f9488f9a04ee234ffe110d0f38b1b9f89877647
student <student@gnu.ac.in>
2023-07-14 12:33:03 +0530

    date: 14/07/2023

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,28 @@
+import java.time.Year
+
+fun main(){
+    var a=11
+    println("Integer value: $a")
+
+    var b=46.32
+    println("Double value: $b")
+
+    var c=1.1
+    println("Float value: $c")
+
+    var d=1234567
+    println("Long value: $d")
+
+    var e=-7
+    println("Short value: $e")
+
+    val zChar: Char = 'a'
+    println("Character value: $zChar")
+
+    val j: String = "Gojo"
+    println("String value: $j")
+
+    val k: Boolean = true
+    println("Boolean value: $k")
+
+}
